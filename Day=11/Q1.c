15ea4b777c4eb540e096c5a74a4103ef61dee713
shivambansal6398-lgtm <shivambansal6398@gmail.com>
2026-06-11 20:56:41 +0530

    Day=11 uploaded

---
+++ Day=11/Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int sum(int a,int b) {
+    return a+b;
+}
+int main(){
+    int n1,n2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &n1,&n2);
+    printf("Sum=%d",sum(n1,n2));
+    return 0;
+}
---
+++ Day=11/Q3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int isprime(int n) {
+    if(n<=1)
+        return 0;
+
+    for(int i=2;i<n;i++){
+        if(n%i==0)
+            return 0;
+    }
+    return 1;
+}
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(isprime(num))
+        printf("prime number.");
+    else
+        printf("not a prime number.");
+    return 0;
+}
