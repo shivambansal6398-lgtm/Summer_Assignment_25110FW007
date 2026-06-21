1c79fdce70ec800fc21d3355c55be7b05af8df45
shivambansal6398-lgtm <shivambansal6398@gmail.com>
2026-06-21 15:30:20 +0530

    Day=21 uploaded

---
+++ Day=21/Q2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[100];
+    int i,length;
+    char temp;
+
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+
+    length=strlen(str);
+
+    if(str[length-1]=='\n'){
+        str[length-1]='\0';
+        length--;
+    }
+
+    for(i=0;i<length/2;i++){
+        temp=str[i];
+        str[i]=str[length-i-1];
+        str[length-i-1]=temp;
+    }
+
+    printf("Reversed string: %s\n",str);
+    return 0;
+}
---
+++ Day=21/Q3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int vowels=0,consonants=0,i;
+
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;str[i]!='\0';i++){
+        char ch=str[i];
+
+        if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')){
+            if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' ||
+                ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                vowels++;
+            }else{
+                consonants++;
+            }
+        }
+    }
+
+    printf("Number of vowels = %d\n",vowels);
+    printf("Number of consonants = %d\n",consonants);
+    return 0;
+}
